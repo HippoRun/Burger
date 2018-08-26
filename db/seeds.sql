f9acb8367347515f54600b7795e547446ded08c8
HippoRun <mayradlopezh@gmail.com>
2018-08-26 17:44:11 -0600

    seeds added

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES ('Cheeseburger', false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ('Veggie Burger', false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ('Chicken Burger', false);
